cb0f12c81c3dc8c73843ccd4b9d4aecd60074b8d
yunusikezer <136835645+yunusikezer@users.noreply.github.com>
2023-08-04 12:04:17 +0300

    create_a_image_list_file.py

--- prepare_data/create_a_image_list_file.py
+++ prepare_data/create_a_image_list_file.py
@@ -1,7 +1,8 @@
 import os
 
 
-alpaca_id = '/m/0pcr'
+SignStore = '/m/0pcr'
+Utility Pole Information Plate = '/m/1pcr'
 
 train_bboxes_filename = os.path.join('.', 'oidv6-train-annotations-bbox.csv')
 validation_bboxes_filename = os.path.join('.', 'validation-annotations-bbox.csv')
@@ -16,7 +17,7 @@ for j, filename in enumerate([train_bboxes_filename, validation_bboxes_filename,
         line = f.readline()
         while len(line) != 0:
             id, _, class_name, _, x1, x2, y1, y2, _, _, _, _, _ = line.split(',')[:13]
-            if class_name in [alpaca_id] and id not in image_list_file_list:
+            if class_name in [SignStore_id] or [Utility Pole Information Plate_id] and id not in image_list_file_list:
                 image_list_file_list.append(id)
                 with open(image_list_file_path, 'a') as fw:
                     fw.write('{}/{}\n'.format(['train', 'validation', 'test'][j], id))
